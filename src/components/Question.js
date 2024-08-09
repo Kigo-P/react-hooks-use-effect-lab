e8f12af14d1449be0323d43f8870c36b1911d07d
Kigo-P <peterkigo33@gmail.com>
2024-08-09 06:12:50 +0300

    last commit

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+ 
+   useEffect(()=>{
+    4
+      return
+    }
+    const timer = setTimeout(()=>
+       setTimeRemaining(function(timeRemaining){
+        if(timeRemaining <=10 && timeRemaining >= 0){
+          return timeRemaining -1
+        }
+       }),1000)
+      
+      return function cleanup(){
+        clearTimeout(timer)
+      }
+
+   },[timeRemaining])
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
